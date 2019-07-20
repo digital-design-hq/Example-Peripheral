4a7b91430e7fdcce0b60768c0ef2a7c7426fcd8a
Reuben020 <32236404+Reuben020@users.noreply.github.com>
2019-07-20 00:08:20 -0600

    Added folder structure.

--- sim/compile_sv.f
+++ sim/compile_sv.f
@@ -3,13 +3,13 @@
 
 -sv 
 
-../rtl/single_port_memory.sv
-../rtl/peripheral_core.sv
-../rtl/peripheral_register_interface.sv
-../rtl/peripheral_native_register_interface.sv
-../rtl/peripheral_memory_interface.sv
-../rtl/avalon_register_adapter.sv
-../rtl/avalon_memory_adapter.sv
-../rtl/peripheral_register_map.sv
-../rtl/peripheral_top.sv
+../rtl/standard_interfaces/peripheral_register_interface.sv
+../rtl/standard_interfaces/peripheral_memory_interface.sv
+../rtl/standard_adapters/avalon_register_adapter.sv
+../rtl/standard_adapters/avalon_memory_adapter.sv
+../rtl/standard_part_library/single_port_memory.sv
+../rtl/peripheral_core/peripheral_native_register_interface.sv
+../rtl/peripheral_core/peripheral_register_map.sv
+../rtl/peripheral_core/peripheral_core.sv
+../rtl/peripheral_core/peripheral_top.sv
 peripheral_tb.sv
